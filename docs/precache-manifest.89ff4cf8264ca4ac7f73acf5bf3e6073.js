6d318dade0a641808a0b2287b81f32f6684af95c
Razvan Cristea <razcristea@gmail.com>
2019-12-02 15:05:37 +0200

    build deployment

---
+++ docs/precache-manifest.89ff4cf8264ca4ac7f73acf5bf3e6073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d690b8c8db11ae3d44324ff3b448d1e",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4318475e0a1475b6a5ce",
+    "url": "/react-todo/static/css/main.859ac774.chunk.css"
+  },
+  {
+    "revision": "76dc08bf675e6010610b",
+    "url": "/react-todo/static/js/2.612ce9c1.chunk.js"
+  },
+  {
+    "revision": "4318475e0a1475b6a5ce",
+    "url": "/react-todo/static/js/main.1a8d79a9.chunk.js"
+  },
+  {
+    "revision": "7a1f9892c9ff4e113a3d",
+    "url": "/react-todo/static/js/runtime-main.aed2e04c.js"
+  }
+]);
